de6f5946da80970473ae9b43b7e56ecdd7c58074
Amol Kobal <amol_kobal@rediffmail.com>
2019-10-30 16:26:55 +0530

    Added Validations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,7 @@ class ArticlesController < ApplicationController
     end
         
     def new
+        @article = Article.new
     end
 
     def show
@@ -15,8 +16,11 @@ class ArticlesController < ApplicationController
         # render plain: params[:article].inspect
         @article = Article.new(article_params)
 
-        @article.save()
-        redirect_to @article
+        if @article.save()
+            redirect_to @article
+        else
+            render 'new'
+        end
 
     end
 
